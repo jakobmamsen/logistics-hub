ba84acf247f6bd6fe69600110cd93499c79ebcff
FPL <jakobmmamsen@gmail.com>
2026-09-15 19:43:43 +0000

    Add Card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export const Card = ({ 
+  children, 
+  className = '', 
+  title,
+  ...props 
+}) => {
+  return (
+    <div 
+      className={`bg-white rounded-lg shadow p-6 ${className}`}
+      {...props}
+    >
+      {title && <h3 className="text-lg font-semibold mb-4">{title}</h3>}
+      {children}
+    </div>
+  );
+};
+
+export default Card;
